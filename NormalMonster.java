ee59789d9b52e08c93e651550f042442047a879b
nergissus <nergiscelen@gmail.com>
2015-05-02 02:18:15 +0300

    Create NormalMonster.java

---
+++ NormalMonster.java
@@ -0,0 +1,12 @@
+import java.awt.Image;
+
+
+public class NormalMonster extends Monster {
+
+	private Image graphics_normal;
+	
+	public void moveRandomly() { //Normal monster moves randomly
+		
+		
+	}
+}

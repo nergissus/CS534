70620abd31d7844bd6aa52276295e28d4337422e
nergissus <nergiscelen@gmail.com>
2015-05-02 02:11:32 +0300

    Create Bomb.java

---
+++ Bomb.java
@@ -0,0 +1,22 @@
+import java.awt.Image;
+
+import javax.management.timer.Timer;
+
+
+public class Bomb {
+
+	private Image graphic;
+	private Timer timer;
+    private int type;
+    
+    public Bomb() {
+    	
+    }
+    
+    public void explode() {  //bomb explodes when time is up
+    	
+    	    	
+    }
+    
+   
+}

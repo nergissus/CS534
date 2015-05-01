40fb2425ed0a55d78a169d80d8ad319a5e0991ab
nergissus <nergiscelen@gmail.com>
2015-05-02 02:18:45 +0300

    Create StaticObject.java

---
+++ StaticObject.java
@@ -0,0 +1,14 @@
+import java.awt.Image;
+
+
+public class StaticObject {
+
+	private Image graphic;
+	private boolean type; //type can be exit door, empty location or other static objects such as bushes, walls etc.
+	
+	public void setGraphic(Image img) {
+		
+		
+	}
+	
+}

914109f78022fa3c7439a0ee4e33bacac3efe6bb
nergissus <nergiscelen@gmail.com>
2015-05-02 02:16:05 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+
+
+public class MainActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameUI m = new GameUI();
+        m.show();
+
+	}
+
+}

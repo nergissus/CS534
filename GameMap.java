0cdd6cc87bc750a7e614d68ef39aff92bc0f8eca
nergissus <nergiscelen@gmail.com>
2015-05-02 02:15:08 +0300

    Create GameMap.java

---
+++ GameMap.java
@@ -0,0 +1,37 @@
+
+public class GameMap {
+
+	private ExitDoor door;
+	GameUI gameUI;
+	
+	
+	public void removeObjectFromGUI(Object o) {
+		
+		if(o instanceof Monster) {
+			
+			
+			
+		}
+		
+		else if(o instanceof MainCharacter) {
+			
+		}
+		
+		else if(o instanceof StaticObject) {
+			
+			
+		}
+	}
+	
+	public void addBomb(Bomb b) {  //adds bomb to GUI where character throws to
+		
+	}
+	
+	public void setDoorOpen() {
+		
+		
+	}
+	
+	
+
+}

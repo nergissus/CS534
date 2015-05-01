e949e998bfe54b9b822959ef12ee41f8877a2b5f
nergissus <nergiscelen@gmail.com>
2015-05-02 02:19:12 +0300

    Create SuperMonster.java

---
+++ SuperMonster.java
@@ -0,0 +1,12 @@
+import java.awt.Image;
+
+
+public class SuperMonster extends Monster {
+
+	private Image graphics_super;
+	
+	public void moveTowardsPlayer() {  //Super monster moves towards to main character
+		
+		
+	}
+}
